cae685f20153a804ea125bd9e0bb32ee24b33240
anacarolestrela <94330071+anacarolestrela@users.noreply.github.com>
2024-02-27 15:55:34 -0300

    Create 1001.cpp

---
+++ begginer/1001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int a, b;
+    int x = 0;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    x= a + b;
+    printf("X = %d\n", x);
+    return 0;
+}
