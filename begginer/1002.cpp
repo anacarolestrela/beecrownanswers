55de0d7666ca1a9eed4167acd90e2d0d7e025e09
anacarolestrela <94330071+anacarolestrela@users.noreply.github.com>
2024-02-27 15:56:14 -0300

    Create 1002.cpp

---
+++ begginer/1002.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio> 
+ 
+using namespace std;
+ 
+int main() {
+    double a, r;
+    scanf("%lf", &r);
+    a = 3.14159 * r * r;
+    printf("A=%.4lf\n", a);
+    return 0;
+}
