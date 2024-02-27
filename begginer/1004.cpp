94b8c60aa10c95a44e4c6890b045453f6444ac95
anacarolestrela <94330071+anacarolestrela@users.noreply.github.com>
2024-02-27 15:57:21 -0300

    Create 1004.cpp

---
+++ begginer/1004.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int a,b, prod;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    prod = a*b;
+    printf("PROD = %d\n", prod);
+    return 0;
+}
