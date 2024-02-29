e6429603bd7cb2c9b6875e2e1597edd446982b97
anacarolestrela <94330071+anacarolestrela@users.noreply.github.com>
2024-02-29 19:59:10 -0300

    Create 1017.cpp

---
+++ begginer/1017.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int t,s;
+    double f,d;
+    scanf("%d",&t);
+    scanf("%d",&s);
+    d=t*s;
+    f= d/12.0;
+    printf("%.3lf\n",f);
+ 
+    return 0;
+}
